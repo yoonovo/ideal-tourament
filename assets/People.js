6f93339647bac2d01b4b114c3858b993e14176ed
윤희수 <hsyoon@iparking.co.kr>
2021-06-07 13:40:10 +0900

    파일생성

---
+++ assets/People.js
@@ -0,0 +1,8 @@
+var DATA = [
+    { "name": "A", "imgUrl": ""},
+    { "name": "B", "imgUrl": ""},
+    { "name": "C", "imgUrl": ""},
+    { "name": "D", "imgUrl": ""},
+    { "name": "E", "imgUrl": ""},
+    { "name": "F", "imgUrl": ""}
+]
